fe73021f661f1506a2795a3912e435f818715032
wellington18m <34990818+wellington18m@users.noreply.github.com>
2019-03-05 00:15:10 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ let file = ".jpg";
 let imgDir = "worship/"
 let images = ["img1", "img2","img3","img4","cover","img5","img6",
                "img7","img8","img9","img10","img11","img12",
-               "img13","img14","img15","img16","img17","img18",
+               "img13","img14","img15","img16","img17","img18", 
                "img19","img20","img21","img22","img23","img24",
                "img25","img26","img27","img28"
 ];
